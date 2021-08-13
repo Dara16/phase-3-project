46229a17f7ef6a3c4603f3c7cb114c0439869f75
dara <just4oludee@yahoo.com>
2021-08-13 17:34:09 -0400

    routes created and tested

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -13,37 +13,126 @@ class Application
 
     end
 
-    # Resource routes
+    # OCCUPANTS RESOURCE
 
-    if req.path == '/occupants' && req.get?
-      occupants = Occupant.all
-      return [
-        200, { 'Content-Type' => 'application/json' }, 
-        [ occupants.to_json ]
-      ]
+    # Occupants index
 
-    elsif req.path == '/apartments' && req.get?
-      apartments = Apartment.all
-      return[
-        200, { 'Content-Type' => 'application/json' },
-        [ apartments.to_json ]
-      ]
+    if req.path == ('/occupants') && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [Occupant.all.to_json]]
+    end
 
-    elsif req.path == '/tenements' && req.get?
-      tenements = Tenement.all
-      return[
-        200, { 'Content-Type' => 'application/json' },
-        [ tenements.to_json ]
-      ]
+    # Occupants create
 
-    else 
-      res.write "Path not found"
-      
+    if req.path == ('/occupants') && req.post?
+      body = JSON.parse(req.body.read)
+      occupant = Occupant.create(body)
+      return [201, {'Content-Type' => 'application/json'}, [occupant.to_json]]
+    end
+
+    # Occupants show
+
+    if req.path.match('/occupants/') && req.get?
+      id = req.path.split('/')[2]
+      begin
+        occupant = Occupant.find(id)
+        return [200, {'Content-Type' => 'application/json'}, [occupant.to_json]]
+      rescue
+        return [404, {'Content-Type' => 'application/json'}, [{message: "Occupant not found"}.to_json]]
+      end
+    end
+
+    # Occupants update
+
+    if req.path.match('/occupants/') && req.patch?
+      id = req.path.split('/')[2]
+      body = JSON.parse(req.body.read)
+      begin
+        occupant = Occupant.find(id)
+        occupant.update(body)
+        return [202, {'Content-Type' => 'application/json'}, [occupant.to_json]]
+      end
+    end
+
+    # Occupants delete
+
+    if req.path.match('/occupants/') && req.delete?
+      id = req.path.split('/')[2]
+      begin
+        occupant = Occupant.find(id)
+        occupant.destroy
+        return [200, {'Content-Type' => 'application/json'}, [{message: "Occupant Destroyed"}.to_json]]
+      rescue
+        return [404, {'Content-Type' => 'application/json'}, [{message: "Occupant not found"}.to_json]]
+      end
     end
 
 
-  res.finish
+    # TENEMENTS RESOURCE
+
+    # Tenements index
+
+    if req.path == ('/tenements') && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [Tenement.all.to_json]]
+    end
+
+    # Tenements show
+
+    if req.path.match('/tenements/') && req.get?
+      id = req.path.split('/')[2]
+      begin
+        tenement = Tenement.find(id)
+
+        return [200, {'Content-Type' => 'application/json'}, [tenement.as_json(include: :apartments).to_json]]
+      rescue
+        return [404, {'Content-Type' => 'application/json'}, [{message: "Building not found"}.to_json]]
+      end
+    end
+
+    # Tenements Update
+
+    if req.path.match('/tenements/') && req.patch?
+      id = req.path.split('/')[2]
+      body = JSON.parse(req.body.read)
+      begin
+        tenement = Tenement.find(id)
+        tenement.update(body)
+        return [202, {'Content-Type' => 'application/json'}, [tenement.to_json]]
+      end
+    end
+
+    # APARTMENTS RESOURCE
+
+    # Apartments index
+
+    if req.path == ('/apartments') && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [Apartment.all.to_json]]
+    end
+
+    # Apartments show
+
+    if req.path.match('/apartments/') && req.get?
+      id = req.path.split('/')[2]
+      begin
+        apartment = Apartment.find(id)
+      
+        return [200, {'Content-Type' => 'application/json'}, [apartment.as_json(include: :occupants).to_json]]
+      rescue
+        return [404, {'Content-Type' => 'application/json'}, [{message: "Apartment not found"}.to_json]]
+      end
+    end
+
+    # Apartments update
+
+    if req.path.match('/apartments/') && req.patch?
+      id = req.path.split('/')[2]
+      body = JSON.parse(req.body.read)
+      begin
+        apartment = Apartment.find(id)
+        apartment.update(body)
+        return [202, {'Content-Type' => 'application/json'}, [apartment.to_json]]
+      end
+    end
 
   end
-
 end
+    
